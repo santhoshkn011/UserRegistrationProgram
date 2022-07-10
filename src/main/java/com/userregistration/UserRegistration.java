1744b6d157c226b9cded25098bcb8bf4ec6609a9
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-07-10 14:51:19 +0530

    Added: Welcome Message.

---
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -0,0 +1,7 @@
+package com.userregistration;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration problems.");
+    }
+}
