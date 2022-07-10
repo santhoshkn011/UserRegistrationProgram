481d4bb76d70bbbe0d9dc25e00bc2e48e3a558b7
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-07-10 16:55:42 +0530

    Added: Checking Email Samples

--- src/main/java/com/userregistration/Operations.java
+++ src/main/java/com/userregistration/Operations.java
@@ -6,91 +6,20 @@ import java.util.regex.Pattern;
 
 public class Operations {
     static Scanner sc = new Scanner(System.in);
-    // taking ValidFirstName method
-    public static boolean validFirstName() {
-        System.out.print("Enter the first name: ");
-        String name = sc.next();
-        //regex method to check the first name(First letter is Cap and has min 3 characters)
-        String regex = "^[A-Z]{1}[a-z]{2,}$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher match = pattern.matcher(name);
-        boolean result = match.matches();
-        // if condition is to check the first name is valid or not
-        if (result) {
-            System.out.println("Your First name is valid");
-        }
-        else {
-            System.out.println("Your First name is invalid");
-        }
-        return result;
-    }
-    //method to check the last name
-    public static boolean validLastName() {
-        System.out.print("Enter the Last name: ");
-        String nameLast = sc.next();
-        //regex function is used to check the first name
-        String regix = "^[A-Z]{1}[a-z]{2,}$";
-        Pattern p = Pattern.compile(regix);
-        Matcher m = p.matcher(nameLast);
-        boolean result = m.matches();
-        // if condition is to check the Last name is valid or not
-        if (result)
-            System.out.println("Your Last name is valid");
-        else
-            System.out.println("Your Last name is invalid");
-        return result;
-    }
+
     // Method to check email.
-    public static boolean validEmailAddress(){
+    public static boolean validEmailAddress() {
         System.out.print("Enter the Email Address:  ");
-        String emailId = sc.next();
-        /*
-        E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in) with precise @ and . positions
-        */
-        String regex = "^[a-z]{3}[a-zA-z.]{0,}+@+bl.co+[a-z.]{0,}$";
+        String emailAddress = sc.next();
+        String regex = "^(?!.*@.*@)[a-z+_-]+(.[a-z0-9])*@[a-z0-9]+(.[a-z])*.[a-z]{2,}$";
         Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(emailId);
+        Matcher m = p.matcher(emailAddress);
         boolean result = m.matches();
         // if condition is to check the email address is valid or not
         if (result)
             System.out.println("Your Email Address is valid");
         else
             System.out.println("Your Email Address is invalid");
-        return result;
-    }
-    // Method to check mobile number.
-    public static boolean validMobileNumber(String mobNumber) {
-        String regex = "^[0-9]{2}\\s{0,1}[0-9]{10,}$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(mobNumber);
-        boolean result = matcher.matches();
-        // if condition is to check the email address is valid or not
-        if (result)
-            System.out.println("Your Mobile Number is valid");
-        else
-            System.out.println("Your Mobile Number is invalid");
-        return result;
-    }
-    // Method for the password validation
-    public static boolean validPassword(){
-        System.out.print("Enter the Password: ");
-        String password = sc.next();
-        /*
-         * (?=.*[A-Z]) represents an upper case character that must occur at least once.
-         * (?=.*[0-9]) represents a digit must occur at least once.
-         * (?+.*[@#$%^&*()] represent the special symbol at least once.
-         * (?=.*[a-zA-z0-9]) represents a lower case character or number  must occur at least once.
-         * {8,} represents at least 8 or more characters.
-         */
-        String regex = "^(?=.*[A-z])(?=.*[0-9])(?=.*[@#$%^&*()-+=])([a-zA-Z0-9@._-]).{8,}$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(password);
-        boolean result = matcher.matches();
-        // if condition is to check the password is valid or not
-        if (result)
-            System.out.println("This is the valid password");
-        else
-            System.out.println("Your Password is invalid.");
         return result;
     }
 }
--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -1,18 +1,3 @@
-/*
-As a User need to follow pre-defined Password rules.
-Rule1
- – minimum 8
-
-Rule2
-– Should have at least 1 Upper Case
-
-Rule3
-– Should have at least 1 numeric number in the password
-
-Rule4 – Has exactly 1 Special Character
-
-Characters - NOTE – All rules must be passed
- */
 package com.userregistration;
 import java.util.Scanner;
 
@@ -25,31 +10,51 @@ public class UserRegistration {
         int choose;
         int select;
         do {
-            System.out.println("Please Select the Options:\n1. Validation of First Name\n2. Validation of Last Name" +
-                    "\n3. Email Address validation.\n4. Mobile Number Validation\n5. Password Validation");
+            System.out.println("Press 1: Email Address Validation\n2. Exit");
             choose = sc.nextInt();
             switch (choose) {
                 case 1:
-                    operations.validFirstName();
-                    break;
-                case 2:
-                    operations.validLastName();
-                    break;
-                case 3:
                     operations.validEmailAddress();
-                    break;
-                case 4:
-                    operations.validMobileNumber("91 9704329740");
-                    break;
-                case 5:
-                    operations.validPassword();
                     break;
                 default:
                     System.out.println("Invalid: Enter valid details.");
                     break;
             }
-            System.out.println("If you want to continue press 1 or to exit press 6");
+            System.out.println("If you want to continue press 1 or to exit press 2");
             select = sc.nextInt();
-        } while (select != 6);
+        } while (select != 2);
     }
 }
+
+/*
+Should clear all email samples
+Sample Emails to Test
+A. Valid Emails
+• 1. abc@yahoo.com,
+• 2. abc-100@yahoo.com,
+• 3. abc.100@yahoo.com
+• 2. abc111@abc.com,
+• 4. abc-100@abc.net,
+• 5. abc.100@abc.com.au
+• 6. abc@1.com,
+• 7. abc@gmail.com.com
+• 8. abc+100@gmail.com
+
+
+B. Invalid Emails (TLD - Top Level Domains)
+1. abc – must contains “@” symbol
+2. abc@.com.my – tld can not start with dot “.”
+3. abc123@gmail.a – “.a” is not a valid tld, last tld must contains at least two
+characters
+4. abc123@.com – tld can not start with dot “.”
+5. abc123@.com.com – tld can not start with dot “.”
+6. .abc@abc.com – email’s 1st character can not start with “.”
+7. abc()*@gmail.com – email’s is only allow character, digit, underscore and dash
+8. abc@%*.com – email’s tld is only allow character and digit
+9. abc..2002@gmail.com – double dots “.” are not allow
+10. abc.@gmail.com – email’s last character can not end with dot “.”
+11. abc@abc@gmail.com – double “@” is not allow
+12. abc@gmail.com.1a -email’s tld which has two characters can not contain digit
+13. abc@gmail.com.aa.au - cannot have multiple email’s tld
+*/
+
